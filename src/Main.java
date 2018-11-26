fc2abc241358662a6ba32326c47c2499a16b5aae
Ayaka Toma <e175737@ie.u-ryukyu.ac.jp>
2018-11-26 23:54:34 +0900

    初期コード

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが");
-            System.out.println(e.getMessage());
-        }
+        String str = null;
+        System.out.println(str.length());
     }
 }
