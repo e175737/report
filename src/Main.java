8db126681f5874fc353d624ce1dcccc83da1710c
Ayaka Toma <e175737@ie.u-ryukyu.ac.jp>
2018-11-26 23:36:11 +0900

    初期コード

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.println(e.getMessage());
-        }
+        String str = null;
+        System.out.println(str.length());
     }
 }
